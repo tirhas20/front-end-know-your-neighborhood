b569b46b054ead279e2d47b973dc2555a379fbce
Tirhas Gebreyohannes <tirhastse@gmail.com>
2022-02-14 12:23:02 -0800

    change image

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+
 const domain = process.env.REACT_APP_AUTH0_DOMAIN
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 const audience = process.env.REACT_APP_API_AUDIENCE
